fc5a00db65fa0d7b2ee7737ff5111382636e5337
aurel.dorin.tatar@reea.net <aurel.dorin.tatar@reea.net>
2026-02-23 05:35:33 +0200

    seed: add one regular user per company

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Company;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
@@ -17,5 +18,31 @@ class UserSeeder extends Seeder
                 'password' => Hash::make('password'),
             ]
         );
+
+        // One regular user per company
+        $companyUsers = [
+            'nod'       => ['name' => 'User NOD',       'email' => 'user@nod.ro'],
+            'paintball' => ['name' => 'User Paintball',  'email' => 'user@paintball.ro'],
+        ];
+
+        $companies = Company::withoutGlobalScopes()->get();
+
+        foreach ($companies as $index => $company) {
+            $userData = array_values($companyUsers)[$index] ?? null;
+            if (! $userData) {
+                continue;
+            }
+
+            $user = User::firstOrCreate(
+                ['email' => $userData['email']],
+                [
+                    'name'     => $userData['name'],
+                    'password' => Hash::make('password'),
+                ]
+            );
+
+            // Assign only their own company (idempotent)
+            $user->companies()->syncWithoutDetaching([$company->id]);
+        }
     }
 }
